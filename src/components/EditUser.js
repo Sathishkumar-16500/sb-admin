a6c2fe3ac98f584c9f62eca188a3d4c00329466b
Sathishkumar-16500 <sathishr16500@gmail.com>
2022-07-22 17:20:21 +0530

    Update EditUser.js

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -3,7 +3,7 @@ import {useParams} from 'react-router-dom'
 import {useSearchParams} from 'react-router-dom'
 function EditUser() {
     let params=useParams();
-    const[SearchParams,setSearchParams]= useSearchParams();
+    // const[SearchParams,setSearchParams]= useSearchParams();
     console.log(SearchParams.get)
     console.log(params)
   return (
